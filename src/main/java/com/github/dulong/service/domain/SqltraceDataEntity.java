6d5e07d50e6a60ae008b9ad1318dc40baf532dc4
dulong <duloong@foxmail.com>
2019-04-11 10:53:54 +0800

    SqltraceDataEntity

---
+++ src/main/java/com/github/dulong/service/domain/SqltraceDataEntity.java
@@ -0,0 +1,22 @@
+package com.github.dulong.service.domain;
+
+public class SqltraceDataEntity {
+
+    private long sqlTraceId;
+    private String blameMetricName;
+    private String uri;
+    private long sqlId;
+    private String metricName;
+    private int callCount;
+    private float minTime;
+    private float maxTime;
+    private long applicationId;
+    private String parameters;
+    private float totalTime;
+    private float avgTime;
+    private String sqlInfo;
+    private long recordTime;
+    private long agentRunId;
+    private long userId;
+
+}
